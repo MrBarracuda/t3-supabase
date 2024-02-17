a562c270761ee37101c0a31648821b0c384b492f
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-17 23:57:45 +0200

    feat: create profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+
+export default function Profile() {
+  return (
+    <>
+      <MaxWidthWrapper className="py-20">profile</MaxWidthWrapper>
+    </>
+  );
+}
