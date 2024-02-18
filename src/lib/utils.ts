e199ca56e5bbca5ba66436683b61f34dbc8e453f
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-18 20:21:53 +0200

    chore: create formatPrice function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,12 +6,30 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const createUrl = (
+export function createUrl(
   pathname: string,
   params: URLSearchParams | ReadonlyURLSearchParams,
-) => {
+) {
   const paramsString = params.toString();
   const queryString = `${paramsString.length ? "?" : ""}${paramsString}`;
 
   return `${pathname}${queryString}`;
-};
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "UAH";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {},
+) {
+  const { currency = "USD", notation = "compact" } = options;
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
