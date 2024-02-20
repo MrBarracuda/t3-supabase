843a0d8450a444dd3f9a2a985aef52a6dc36109e
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-20 21:49:35 +0200

    feat: show user accessories/sale pages will be added later

--- src/app/(product)/[product_category]/page.tsx
+++ src/app/(product)/[product_category]/page.tsx
@@ -1,19 +1,37 @@
-"use client";
-
 import { PRODUCT_CATEGORIES } from "@/config";
 import { notFound } from "next/navigation";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 interface ProductPageProps {
   params: {
     product_category: "accessories" | "men" | "women" | "kids" | "sale";
   };
 }
-export default function ProductPage({ params }: ProductPageProps) {
-  const is = PRODUCT_CATEGORIES.find(
+export default function Product({ params }: ProductPageProps) {
+  const currentCategory = PRODUCT_CATEGORIES.find(
     (product) => product.title === params.product_category,
   );
 
-  if (is === undefined) return notFound();
+  if (currentCategory === undefined) {
+    return notFound();
+  }
 
-  return <div>{is.title}</div>;
+  if (
+    currentCategory.title === "accessories" ||
+    currentCategory.title === "sale"
+  ) {
+    return (
+      <MaxWidthWrapper className="py-20">
+        <h1 className="text-3xl">Work in progress. Stay in touch</h1>
+      </MaxWidthWrapper>
+    );
+  }
+
+  return (
+    <>
+      <MaxWidthWrapper className="py-20">
+        <h1 className="text-3xl">{currentCategory.title}</h1>
+      </MaxWidthWrapper>
+    </>
+  );
 }
