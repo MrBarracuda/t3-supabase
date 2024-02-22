b88fed803387eaf975c937a292d93081533ad98e
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-22 14:25:32 +0200

    fix: warning extra attributes from the server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/*suppressHydrationWarning if getting errors*/}
       <body
         className={cn(
