83ae2870f9e2cf7ced4b5f970ed9eb7a7627d08d
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-28 15:28:25 +0200

    feat: implement product item navigation

--- src/app/(product)/[product_category]/page.tsx
+++ src/app/(product)/[product_category]/page.tsx
@@ -5,13 +5,14 @@ import { notFound } from "next/navigation";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { api } from "@/trpc/react";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
-interface ProductPageProps {
+interface ProductProps {
   params: {
     product_category: "accessories" | "men" | "women" | "kids" | "sale";
   };
 }
-export default function Product({ params }: ProductPageProps) {
+export default function Product({ params }: ProductProps) {
   const currentCategory = PRODUCT_CATEGORIES.find(
     (product) => product.title === params.product_category,
   );
@@ -19,6 +20,11 @@ export default function Product({ params }: ProductPageProps) {
   if (currentCategory?.title === undefined) {
     return notFound();
   }
+
+  // TODO: handle {gender: property} eq to undefiend causing request to fail
+  const { data: products } = api.product.getAllByCategory.useQuery(
+    currentCategory.title,
+  );
 
   const isSaleOrAccessoriesPage =
     currentCategory.title === "accessories" || currentCategory.title === "sale";
@@ -31,20 +37,24 @@ export default function Product({ params }: ProductPageProps) {
     );
   }
 
-  // TODO: handle {gender: property} eq to undefiend causing request to fail
-  const { data: products } = api.product.getAllByCategory.useQuery(
-    currentCategory.title,
-  );
+  if (products === undefined || products.length === 0) {
+    return <>Loading...</>;
+  }
 
   return (
     <>
       <MaxWidthWrapper className="py-20">
         <h1 className="text-3xl">{currentCategory.title}</h1>
         <div>
-          {products?.map((product) => (
+          {products.map((product) => (
             <div className="grid gap-2" key={product.id}>
               <Separator />
-              <div className="font-bold capitalize">{product.title}</div>
+              <Link
+                href={`/${currentCategory.title}/${product.id}`}
+                className="font-bold capitalize"
+              >
+                {product.title}
+              </Link>
               <div className="font-medium text-muted-foreground">
                 {product.category}
               </div>
