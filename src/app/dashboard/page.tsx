23428e73affc5e9890025ab890bffb9612e994c1
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-02-28 15:27:04 +0200

    feat: add nav link to create page from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Dashboard() {
   return (
     <>
       <MaxWidthWrapper className="py-20">
-        <h1 className="text-3xl">Dashboard</h1>
+        <div className="grid-2 grid">
+          <h1 className="text-3xl">Dashboard</h1>
+          <Link
+            href="/dashboard/create"
+            className={cn(buttonVariants({ variant: "link" }), "text-lg")}
+          >
+            Create
+          </Link>
+        </div>
       </MaxWidthWrapper>
     </>
   );
